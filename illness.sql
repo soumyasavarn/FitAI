55af54c60134fb49380891964210e5c0a49dc938
zombie-programmer-code <121932403+zombie-programmer-code@users.noreply.github.com>
2024-04-12 00:35:01 +0530

    Add files via upload

---
+++ illness.sql
@@ -0,0 +1,8 @@
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Cardiovascular Disease', TRUE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Type 2 Diabetes', TRUE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Asthma', TRUE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Cancer', FALSE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Hypertension', TRUE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Chronic Kidney Disease', TRUE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Alzheimer''s Disease', TRUE);
+INSERT INTO illness_details (illness_name, chronic) VALUES ('Osteoarthritis', TRUE);
