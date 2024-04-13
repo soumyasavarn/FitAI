a8641de1b149e073c9cda6931322f28b76de9213
Ishan Gupta <134151658+89P13-24@users.noreply.github.com>
2024-04-13 11:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
